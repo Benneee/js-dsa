4ea8fa1551e80cba6b43649adf7bfeeca0b311ca
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2023-09-23 14:20:37 +0100

    Feat: linkedlist methods for push and pop

--- data-structures/LinkedList.js
+++ data-structures/LinkedList.js
@@ -37,8 +37,48 @@ class LinkedList {
         this.tail = this.head
         this.length = 1
     }
+
+    push(value) {
+        const newNode = new Node(value)
+        if (!this.head) {
+            // In this instance, the LinkedList doesn't have any nodes
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+
+        this.length++;
+        return this;
+    }
+
+    pop() {
+        if (!this.head) {
+            return undefined
+        }
+        let pre = this.head
+        let temp = this.head
+
+        while (temp.next) {
+            pre = temp
+            temp = temp.next
+        }
+
+        this.tail = pre
+        this.tail.next = null
+        this.length--
+        if (this.length === 0) {
+            this.head = null
+            this.tail = null
+        }
+
+        return temp;
+    }
 }
 
-let myLinkedList = new LinkedList(4);
+let myLinkedList = new LinkedList(7);
+myLinkedList.push(4);
+myLinkedList.pop();
 
 console.log('list: ', myLinkedList);
